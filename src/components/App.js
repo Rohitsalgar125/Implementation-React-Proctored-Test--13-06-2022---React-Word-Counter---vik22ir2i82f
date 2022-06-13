f3313f8c134ebbc76cfde5727e13b8a807b83f45
Rohitsalgar125 <101690901+Rohitsalgar125@users.noreply.github.com>
2022-06-13 22:51:00 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,72 @@
-import "./styles.css";
-import React, { useState } from "react";
-export default function App() {
-  const [countchar, setcountchar] = useState(0);
-  const [countword, setcountword] = useState(0);
-  const font = { fontSize: "16px" };
-  const [range1, setrange1] = useState(font);
+import React from "react";
+function WordCounter() {
+  const [state, setState] = React.useState({
+    wordCount: 0,
+    charCount: 0
+  });
+  const handleKeyPress = (e) => {
+    const count = e.target.value;
+    const countWords = (count) => {
+      if (count.length === 0) {
+        return 0;
+      } else {
+        count = count.replace(/(^\s*)|(\s*$)/gi, "");
+        count = count.replace(/[ ]{2,}/gi, " ");
+        count = count.replace(/\n /, "\n");
+        return count.split(" ").length;
+      }
+    };
+    setState({
+      wordCount: countWords(count),
+      charCount: count.length
+    });
+  };
+  const [limit, setLimit] = React.useState("50");
+  const [font, setFontSize] = React.useState("16");
   return (
-    <>
-      Font size picker
-      <input
-        type="range"
-        min="16px"
-        max="32px"
-        onclick={function (event) {
-          setrange1(event.target.value);
-        }}
-      />
-      <input type="text" id="char-limit-input" value="50" />
+    <div id="container">
       <div>
-        <textarea
-          onChange={function display(event) {
-            setcountchar(event.target.value.length);
-            setcountword(event.target.value.split(" ").length);
+        <p>Font size picker</p>
+        <input
+          type="range"
+          id="fontSize-input"
+          min="16"
+          max="36"
+          onChange={(event) => {
+            setFontSize(event.target.value);
+            // console.log(event.target.value);
           }}
-          style={{ fontSize: { range1 } }}
-        ></textarea>
+        ></input>
       </div>
-      <div id="word-counter">Total number ofwords written {countword}</div>
-      <div id="char-counter">Total number of characters used {countchar}</div>
-    </>
+      <div>
+        <p>Word limit input </p>
+        <input
+          type="number"
+          id="char-limit-input"
+          defaultValue={limit}
+          onChange={(event) => {
+            setLimit(event.target.value);
+          }}
+        ></input>
+      </div>
+      <textarea
+        style={{ fontSize: `${font}px` }}
+        placeholder="Type here"
+        onChange={handleKeyPress}
+        maxLength={limit}
+      ></textarea>
+      <div id="word-counter">
+        Total number of words written
+        <span className="num"> {state.wordCount}</span>
+      </div>
+      <div id="char-counter">
+        Total number of characters used
+        <span className="num"> {state.charCount}</span>
+      </div>
+    </div>
   );
 }
+function NsProject() {
+  return <WordCounter />;
+}
+export default NsProject;
